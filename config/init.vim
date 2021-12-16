80317e9b60a0217a9181558d13893dd9c3106424
Josa Gesell <josa@gesell.me>
2021-12-16 20:07:43 +0100

    inital commit

---
+++ config/init.vim
@@ -0,0 +1,15 @@
+set rtp+=.
+set rtp+=$PLENARY_DIR
+
+runtime plugin/plenary.vim
+
+if $INIT_FILE =~ ".lua$"
+  luafile $INIT_FILE
+endif
+
+if $INIT_FILE =~ ".vim$"
+  source $INIT_FILE
+endif
+
+lua require('plenary.busted')
+  
---
+++ run/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+if [[ "$VENDOR_DIR" == "" ]]; then
+  VENDOR_DIR=$(mktemp -d)
+  function cleanup {
+    rm -rf "$VENDOR_DIR"
+  }
+  trap cleanup EXIT
+fi
+
+export PLENARY_DIR="$VENDOR_DIR/plenary.nvim.git"
+
+if ! [[ -d "$PLENARY_DIR" ]]; then
+  git clone https://github.com/nvim-lua/plenary.nvim.git $PLENARY_DIR 2> /dev/null
+fi
+
+nvim \
+  --headless \
+  --noplugin \
+  -u ${ROOT_DIR}/config/init.vim \
+  -c "PlenaryBustedDirectory ${SPECS_DIR:-specs} { minimal_init = '${ROOT_DIR}/config/init.vim' }"
+
